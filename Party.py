ff54ba0b0e9c6975604bd84b56a27b94caf380a1
grahamvaith <58489176+grahamvaith@users.noreply.github.com>
2020-12-07 02:44:00 -0500

    updated 12/7 2:43 am

--- Party.py
+++ Party.py
@@ -8,17 +8,18 @@ class Party:
         self.gender_percentages = { "M": 0.0, "F": 0.0, "U": 0.0}
 
     def process_data(self):
-        self.race_percentages["1"] = float(self.races["1"]) / float(self.num_voters) * 100
-        self.race_percentages["2"] = float(self.races["2"]) / float(self.num_voters) * 100
-        self.race_percentages["3"] = float(self.races["3"]) / float(self.num_voters) * 100
-        self.race_percentages["4"] = float(self.races["4"]) / float(self.num_voters) * 100
-        self.race_percentages["5"] = float(self.races["5"]) / float(self.num_voters) * 100
-        self.race_percentages["6"] = float(self.races["6"]) / float(self.num_voters) * 100
-        self.race_percentages["7"] = float(self.races["7"]) / float(self.num_voters) * 100
-        self.race_percentages["9"] = float(self.races["9"]) / float(self.num_voters) * 100
-        self.gender_percentages["M"] = float(self.genders["M"]) / float(self.num_voters) * 100
-        self.gender_percentages["F"] = float(self.genders["F"]) / float(self.num_voters) * 100
-        self.gender_percentages["U"] = float(self.genders["U"]) / float(self.num_voters) * 100
+        if self.num_voters > 0:
+            self.race_percentages["1"] = float(self.races["1"]) / float(self.num_voters) * 100
+            self.race_percentages["2"] = float(self.races["2"]) / float(self.num_voters) * 100
+            self.race_percentages["3"] = float(self.races["3"]) / float(self.num_voters) * 100
+            self.race_percentages["4"] = float(self.races["4"]) / float(self.num_voters) * 100
+            self.race_percentages["5"] = float(self.races["5"]) / float(self.num_voters) * 100
+            self.race_percentages["6"] = float(self.races["6"]) / float(self.num_voters) * 100
+            self.race_percentages["7"] = float(self.races["7"]) / float(self.num_voters) * 100
+            self.race_percentages["9"] = float(self.races["9"]) / float(self.num_voters) * 100
+            self.gender_percentages["M"] = float(self.genders["M"]) / float(self.num_voters) * 100
+            self.gender_percentages["F"] = float(self.genders["F"]) / float(self.num_voters) * 100
+            self.gender_percentages["U"] = float(self.genders["U"]) / float(self.num_voters) * 100
 
     def display(self):
         print("Race Data: " + str(self.races))
--- TestLoadingMain.py
+++ TestLoadingMain.py
@@ -1,13 +1,37 @@
 import csv
 from County import County
-#I wrote a couple of classes you can define a county and in the constructor pass the 3 letters that name the text file (dont need the numbers after)
-#as an example I loaded duval and printed its stats along with the democratic party's stats from there
-duval = County("DUV")
-duval.load_data()
-duval.store_people()
-duval.process_data()
+#Loading counties in and storing in dictionary
+#doesn't store any information about the people themselves (saves ram)
+county_dict = {}
+counties = ['ALA','BAK', 'BAY', 'BRA', 'BRE', 'BRO', 'CAL', 'CHA', 'CIT', 'CLA', 
+            'CLL', 'CLM', 'DAD', 'DES', 'DIX', 'DUV', 'ESC', 'FLA', 'FRA', 'GAD',
+           'GIL', 'GLA', 'GUL', 'HAM', 'HAR', 'HEN', 'HER', 'HIG', 'HIL', 'HOL',
+           'IND', 'JAC', 'JEF', 'LAF', 'LAK', 'LEE', 'LEO', 'LEV', 'LIB', 'MAD',
+           'MAN', 'MRN', 'MRT', 'MON', 'NAS', 'OKA', 'OKE', 'ORA', 'OSC', 'PAL', 'PAS',
+           'PIN', 'POL', 'PUT', 'SAN', 'SAR', 'SEM', 'STJ', 'STL', 'SUM', 'SUW', 'TAY',
+           'UNI', 'VOL', 'WAK', 'WAL', 'WAS']
+for county_name in counties:
+    temp_county = County(county_name)
+    temp_county.load_data()
+    temp_county.process_data()
+    temp_county.create_file()
+    county_dict[county_name] = temp_county
+   
 
-duval.search_people_regular("Graham Vaith", "", "8080 timbermill rd")
-duval.search_people_sorted("Graham Vaith", "",  "8080 timbermill rd")
-duval.create_file()
+
+#when search is input just call it on the county object here
+
+#First search function, can pass directly to
+#Hashmap based
+def search_regular(name, address, county):
+    if not bool(county_dict[county].people_dict):
+        county_dict[county].store_people
+    county_dict[county].search_people_regular(name, address)
+
+
+#Tree based
+def search_sorted(name, address, county):
+    if not bool(county_dict[county].people_dict):
+        county_dict[county].store_people
+    county_dict[county].search_people_sorted(name, address)
 
